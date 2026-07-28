91ebb638227504dddfc58d0f46406c86d7f963ea
Fakhrul066 <fakhrulof02@email.com>
2026-07-28 06:57:13 +0600

    Update portfolio

---
+++ exercises/day-1/data_type.py
@@ -0,0 +1,30 @@
+a=30
+print(type(a))
+c ='a'
+print(type(c))
+com=2-8j
+print(type(com))
+# it is used for single line command
+"""
+this is multiline command
+"""
+
+#aritmatic operation
+
+a=17
+b=83
+print(a+b)
+
+print(a/b)
+
+c="30"
+d=int(c)
+
+sum=b+d
+print(sum)
+
+#to take input from user
+a1=int(input("Enter number 1: "))
+a2=int(input("Enter number 2: "))
+
+print("The sum = ", a1+a2)
---
+++ exercises/day-1/variables.py
@@ -0,0 +1,5 @@
+#variable stores data 
+a=19
+name="Fakhrul"
+print(name)
+print("Hi"," everyone")
