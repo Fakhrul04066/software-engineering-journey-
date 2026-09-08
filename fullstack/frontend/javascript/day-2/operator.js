d6d4dd6871d6789d6dd47de2e146da1cfe13a9a6
Fakhrul066 <fakhrulof02@email.com>
2026-09-09 01:04:35 +0600

    js operators are added

---
+++ fullstack/frontend/javascript/day-2/operator.js
@@ -0,0 +1,33 @@
+// console.log("This is comment");
+let a = 10;
+let b = 20;
+// Arithmetic Operators
+console.log("a= " ,a ," & b= " ,b);
+console.log("Addition: " + (a + b));
+console.log("Subtraction: " + (a - b));
+console.log("Multiplication: " + (a * b));
+console.log("Division: " + (a / b));
+console.log("Modulus: " + (a % b));
+console.log("Increment: " + (++a));
+console.log("Decrement: " + (--b));
+console.log("a power b: " + (a ** b));
+// Assignment Operators
+let c = 10;
+console.log("c= " ,c);
+//comparison operators
+console.log("c==a: " + (c == a));
+console.log("c!=a: " + (c != a));
+let d = "10";
+console.log("c==d: " + (c == d));
+console.log("c===d: " + (c === d));
+//logical operators
+let x = true;
+let y = false;
+console.log("x && y: " + (x && y));
+console.log("x || y: " + (x || y));
+console.log("!x: " + (!x));
+//conditional (ternary) operator
+let age = 18;
+let isAdult = (age >= 18) ? "Yes" : "No";
+console.log("Is adult: " + isAdult);
+
