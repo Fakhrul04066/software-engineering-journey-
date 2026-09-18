bc5e7454270adb4cb00893fc55f26899d3683208
Fakhrul066 <fakhrulof02@email.com>
2026-09-19 01:29:21 +0600

    js script

---
+++ fullstack/frontend/javascript/day-4/guess_number.js
@@ -0,0 +1,6 @@
+let gameNo=30;
+let userNo= prompt("Guess the number: ");
+while(userNo != gameNo){
+    userNo=prompt("You entered the wrong number.Guess again: ");
+}
+console.log("Congress You passed the test");
---
+++ fullstack/frontend/javascript/day-5/string.js
@@ -0,0 +1,2 @@
+let FullName= prompt("Enter Your full name without any space: ");
+console.log("@"+FullName+FullName.length);
